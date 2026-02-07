4400c815e9afed9a7d87eeee6b33ebe66cc5843b
lon40seminarium-kotzian-oskar <benekjebanyprzeztwojamatke@gmail.com>
2026-02-07 20:45:36 +0100

    pls db work clancker new

--- elektronika/settings.py
+++ elektronika/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n!4*p49gd8t3cvr@ugr=7tb8pskuskrnyvej3_!z+23@!ibb$h'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-n!4*p49gd8t3cvr@ugr=7tb8pskuskrnyvej3_!z+23@!ibb$h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
